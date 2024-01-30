f2858f0a6229eaa7b54e6dd2f92004cc735a5c3c
danielmorgan779 <danielm462005@gmail.com>
2024-01-30 10:05:44 -0600

    thinigs
    anoit

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -26,5 +29,15 @@ public class MenuUIHandler : MonoBehaviour
     public void StartNew()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+
+    {
+    #if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+    #else
+        Application.Quit();
+    #endif
     }
 }
